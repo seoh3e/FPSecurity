c1b1d1ecbaf2daab5feac5924247642452ffd3a5
hyeonaut <hyeonaut@gmail.com>
2026-03-24 19:50:44 +0900

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import JSONB
-from .db import Base
+from db import Base
 
 class RawLog(Base):
     __tablename__ = "raw_logs"
